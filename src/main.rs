b2fadf36061b0a7428cc780565fd96da959f258c
Yog <yog.panjarale@gmail.com>
2021-11-19 09:08:26 +0530

    basic get set client

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use mini_redis::{client,Result};
+
+#[tokio::main]
+async fn main()->Result<()> {
+    //open a connection to the mini-server address
+    let mut _client  = client::connect("localhost:8082").await?;
+
+    //set key "hello" to value "world"
+    _client.set("hello", "no".into()).await?;
+
+    // get the value of key "hello"
+    let result =  _client.get("hello").await?;
+
+    // print the value
+    println!("got value from server ; result = {:?}", result);
+    Ok(())
 }
